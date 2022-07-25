d8a6846b82cd2cbac090aeeaaf17b6c3cb7ae4a3
gustavobgt <gustavo_bgt97@hotmail.com>
2022-07-25 13:06:19 -0300

    chore: add enviorment variables setup. Create .env.example file. Add scripts for starting and building the app according to the needed enviorment.

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,59 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const dotenv = require('dotenv');
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  devServer: {
-    port: 3000,
-  },
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-      },
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        loader: "ts-loader",
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "/public/index.html"),
-    }),
-  ],
+module.exports = env => {
+	const currentPath = path.join(__dirname);
+	const basePath = currentPath + '/.env';
+	const envPath = basePath + '.' + env.ENVIRONMENT;
+	const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+	const fileEnv = dotenv.config({ path: finalPath }).parsed;
+
+	const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+		prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+		return prev;
+	}, {});
+
+	return {
+		entry: './src/index.tsx',
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: 'bundle.js',
+		},
+		devServer: {
+			port: 3000,
+		},
+		mode: 'development',
+		module: {
+			rules: [
+				{
+					test: /\.jsx?$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader',
+				},
+				{
+					test: /\.tsx?$/,
+					exclude: /node_modules/,
+					loader: 'ts-loader',
+				},
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader'],
+				},
+			],
+		},
+		resolve: {
+			extensions: ['.tsx', '.ts', '.js'],
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: path.join(__dirname, '/public/index.html'),
+			}),
+			new webpack.DefinePlugin(envKeys),
+		],
+	};
 };

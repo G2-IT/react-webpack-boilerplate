ac560d829f1b3d029b939ec4cc05cf565014cb37
gustavobgt <gustavo_bgt97@hotmail.com>
2022-07-27 08:48:42 -0300

    chore: Configure webpack file using typescript. Add a typescript checker. Transpile ts code using babel typescript preset. Create separate webpack config files for develepoment and production. #4

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
-const container = document.getElementById('app');
-const root = createRoot(container);
-root.render(<App />);
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement as Element);
+
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
